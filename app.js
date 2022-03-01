96544cf9eecbba5299eaee33fa1812b3d0acf458
TA-Sakin <tasakin98@gmail.com>
2022-03-02 02:47:47 +0600

    search box added

--- app.js
+++ app.js
@@ -1,53 +1,62 @@
-window.addEventListener('load', () => {
-    let long;
-    let lat;
-    let temperatureDescription = document.querySelector('.temperature-description');
-    let temperatureDegree = document.querySelector('.temperature-degree');
-    let locationTimezone = document.querySelector('.location-timezone');
-    let locationIcon = document.querySelector('.weather-icon');
-    let temperatureSection = document.querySelector('.temperature');
-    let locationCountry = document.querySelector('.location-country');
-    const temperatureSpan = document.querySelector(".temperature span");
+const API_KEY = `a49f9fae9314178d4f75f686992f9976`;
+const searchTemparature = () => {
+  const city = document.getElementById("city-name").value;
+  document.getElementById("city-name").value = "";
+  if (city !== "") {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        displayWeather(data);
+      });
+  }
+};
+const setinnerText = (id, text) => {
+  document.getElementById(id).innerText = text;
+};
+const displayWeather = (data) => {
+  setinnerText("city", data.name);
+  setinnerText("temperature", data.main.temp);
+  setinnerText("condition", data.weather[0].main);
+  const url = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  const imgIcon = document.getElementById("weather-icon");
+  imgIcon.setAttribute("src", url);
+};
 
+window.addEventListener("load", () => {
+  let long;
+  let lat;
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=a49f9fae9314178d4f75f686992f9976`;
+      fetch(api)
+        .then((response) => response.json())
+        .then((data) => {
+          displayWeather(data);
+          farenheitToCelsius(data);
+        });
+    });
+  }
+});
 
-            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=7eaa6bf3d5b4a121a71678acbfb9c9c5`;
-
-            fetch(api)
-                .then(response => {
-                    return response.json();
-                })
-                .then(data => {
-                    console.log(data);
-                    const {temp} = data.main;
-                    const {description} = data.weather[0];
-                    const {country} = data.sys;
-                    const {icon} = data.weather[0];
-                    //Set DOM elements from the API
-                    temperatureDegree.textContent = temp;
-                    temperatureDescription.textContent = description;
-                    locationTimezone.textContent = data.name;
-                    locationCountry.textContent = country;
-                    locationIcon.innerHTML = `<img src="icons/${icon}.png">`;
-                    // locationIcon.textContent = icn;
-                    //Formula for farenheit to celsius
-                    let celsius = (temp - 32) * (5 / 9);
-                    //Change temperature Farenheit/Celsius
-                    temperatureSection.addEventListener("click", () => {
-                      if (temperatureSpan.textContent === "°F"){
-                        temperatureSpan.textContent = "°C";
-                        temperatureDegree.textContent = Math.floor(celsius);
-                      }
-                      else{
-                        temperatureSpan.textContent = "°F";
-                        temperatureDegree.textContent = temp;
-                      }
-                    })
-                  });
-                });
-              }
-            });
+const farenheitToCelsius = (data) => {
+  let temperatureDegree = document.getElementById("temperature");
+  let temperatureSpan = document.getElementById("change-temp");
+  let temperatureSection = document.getElementById("temp-section");
+  const { temp } = data.main;
+  temperatureDegree.textContent = temp;
+  let farenheit = temp * 1.8 + 32;
+  //Change temperature Farenheit/Celsius
+  temperatureSection.addEventListener("click", () => {
+    if (temperatureSpan.textContent === "°C") {
+      temperatureSpan.textContent = "°F";
+      temperatureDegree.textContent = Math.floor(farenheit);
+    } else {
+      temperatureSpan.textContent = "°C";
+      temperatureDegree.textContent = temp;
+    }
+  });
+};
